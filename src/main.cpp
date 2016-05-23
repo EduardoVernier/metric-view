bc47da8fe07e8513f9eff6c35507bc902a77a753
Eduardo Vernier <efvernier@gmail.com>
2016-05-23 12:55:40 +0200

    Make it compilable on lab. Glut not cooperating.

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,47 @@
+#include <iostream>
 #include <stdio.h>
-#include <GL/glut.h>
-#include <GL/glui.h>
+//#include <GL/glut.h>
+//#include <GL/glui.h>
 
-#include "../include/renderer.h"
-#include "../include/interaction.h"
+//#include "../include/renderer.h"
+//#include "../include/interaction.h"
 #include "../include/EntityTree.h"
 #include "../include/CsvParser.h"
 
 using namespace std;
 
-void initilizeVisualization(int argc, char **argv);
+//void initilizeVisualization(int argc, char **argv);
 EntityTree buildEntityTree(int argc, char **argv);
 
 int main_window;
 
 int main(int argc, char **argv)
 {
-	// Check if a string was inputed
-	if (argc < 2)
-	{
-		cout << ("Usage: runner <dirname>\n");
-		return 1;
-	}
+	//cout << "AAAAAAAAAAA" << endl;
 
-	initilizeVisualization(argc, argv);
+	//initilizeVisualization(argc, argv);
 	EntityTree entityTree = buildEntityTree(argc, argv);
 
-	glutIdleFunc(idle);
-	glutMainLoop();
+	//glutIdleFunc(idle);
+	//glutMainLoop();
 	return 0;
 }
 
+/*
 void initilizeVisualization(int argc, char **argv)
 {
 	// Initialize Glut/Glui
 	glutInit(&argc, argv);
-	glutInitWindowSize(1400,1000);
+ 	glutInitWindowSize(500,500);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); 
 	main_window = glutCreateWindow("MetricView");
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyboard);
 	glutMouseFunc(mouseClick);
 }
+*/
+
 
 EntityTree buildEntityTree(int argc, char **argv)
 {
