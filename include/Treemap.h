23157a67d881ad7dbbd85780168bb961497288aa
Eduardo Vernier <efvernier@gmail.com>
2016-10-23 23:51:47 -0200

    Make Container an inner class.

--- include/Treemap.h
+++ include/Treemap.h
@@ -7,26 +7,34 @@
 
 #include "BaseEntity.h"
 #include "EntityData.h"
-#include "Container.h"
 
 using namespace std;
 
-class Treemap
-{
+class Treemap {
+
 public:
-	Treemap(EntityData *ed, double _width, double _height);
-	void updateSize(double _width, double _height);
-	EntityData* getTree() { return entityData; };
+    //
+    class Container {
+    public:
+        Container(double _width, double _height, double _xOffset, double _yOffset);
+        Container cutArea(double area);
+        void saveCoordinates(vector<BaseEntity *> *row, double scoreSum); // Save coordinates INTO OBJECTS
+
+        double width, height, xOffset, yOffset, shortestEdge;
+    };
+    Treemap(EntityData *ed, double _width, double _height);
+    void updateSize(double _width, double _height);
+    EntityData *getTree() { return entityData; };
 
 private:
-	void treemapMultidimensional(vector<BaseEntity*> *data, double width, double height, double xOffset, double yOffset);
-	void treemapSingledimensional(vector<BaseEntity*> *data, double width, double height, double xOffset, double yOffset);
-	void squarify(vector<BaseEntity*> *data, vector<BaseEntity*> *currentRow, Container container);
-	void normalize(vector<BaseEntity*> *data, int area);
-	int sumScores(vector<BaseEntity*> *data);
-	double sumNormalizedScores(vector<BaseEntity*> *data);
-	int improvesRatio(vector<BaseEntity*> *currentRow, double nextEntity, int length);
+    void treemapMultidimensional(vector<BaseEntity *> *data, double width, double height, double xOffset, double yOffset);
+    void treemapSingledimensional(vector<BaseEntity *> *data, double width, double height, double xOffset, double yOffset);
+    void squarify(vector<BaseEntity *> *data, vector<BaseEntity *> *currentRow, Container container);
+    void normalize(vector<BaseEntity *> *data, int area);
+    int sumScores(vector<BaseEntity *> *data);
+    double sumNormalizedScores(vector<BaseEntity *> *data);
+    int improvesRatio(vector<BaseEntity *> *currentRow, double nextEntity, int length);
 
-	EntityData *entityData;
-	double width, height;
+    EntityData *entityData;
+    double width, height;
 };
--- src/Treemap.cpp
+++ src/Treemap.cpp
@@ -159,3 +159,58 @@ void Treemap::updateSize(double _width, double _height) {
     }
     treemapMultidimensional(&data, (double) _width, (double) _height, 0, 0);
 }
+
+// Inner Class
+// Initialize Container
+Treemap::Container::Container(double _width, double _height, double _xOffset, double _yOffset) {
+
+    width = _width;
+    height = _height;
+    xOffset = _xOffset;
+    yOffset = _yOffset;
+
+    shortestEdge = (width < height) ? width : height;
+}
+
+// Subtract area from this container and return a new container
+Treemap::Container Treemap::Container::cutArea(double area) {
+
+    if (width >= height) {
+        double areaWidth = area / height;
+        double newWidth = width - areaWidth;
+        return Container(newWidth, height, xOffset + areaWidth, yOffset);
+    } else {
+        double areaHeight = area / width;
+        double newHeight = height - areaHeight;
+        return Container(width, newHeight, xOffset, yOffset + areaHeight);
+    }
+}
+
+// Save coordinates that correspond to each element on the treemap into BaseEntity coords member
+void Treemap::Container::saveCoordinates(vector<BaseEntity *> *row, double scoreSum) {
+
+    double subxOffset = xOffset, subyOffset = yOffset; // Offset within the container
+    double areaWidth = scoreSum / height;
+    double areaHeight = scoreSum / width;
+    double coordinates[4];
+
+    if (width >= height) {
+        for (unsigned i = 0; i < row->size(); i++) {
+            coordinates[0] = subxOffset;
+            coordinates[1] = subyOffset;
+            coordinates[2] = subxOffset + areaWidth;
+            coordinates[3] = subyOffset + (*row)[i]->getNormalizedScore() / areaWidth;
+            (*row)[i]->setCoords(coordinates);
+            subyOffset = subyOffset + (*row)[i]->getNormalizedScore() / areaWidth;
+        }
+    } else {
+        for (unsigned i = 0; i < row->size(); i++) {
+            coordinates[0] = subxOffset;
+            coordinates[1] = subyOffset;
+            coordinates[2] = subxOffset + (*row)[i]->getNormalizedScore() / areaHeight;
+            coordinates[3] = subyOffset + areaHeight;
+            (*row)[i]->setCoords(coordinates);
+            subxOffset = subxOffset + (*row)[i]->getNormalizedScore() / areaHeight;
+        }
+    }
+};
