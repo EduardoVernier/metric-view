6af246f4a10414326023b0f6edac18c980e5960a
Eduardo Vernier <efvernier@gmail.com>
2016-11-20 00:04:04 -0200

    Encode similarity in alpha.

--- src/EntityData.cpp
+++ src/EntityData.cpp
@@ -290,9 +290,11 @@ void EntityData::computeMostSimilarPairs(unsigned Rt) {
         }
     }
 
-    // Fill a sorted vector with most similar entities on first indexes
+    // Fill a sorted vector with 1% most similar entities on first indexes
     similarityRank.clear();
-    for (auto it = multimap.begin(); it != multimap.end(); ++it) {
-        similarityRank.push_back(make_pair((*it).second.first, (*it).second.second));
+    int i = 0;
+    for (auto it = multimap.begin(); i < 0.01*multimap.size() ; ++it) {
+        similarityRank.push_back(make_pair((*it).first, make_pair((*it).second.first, (*it).second.second)));
+        ++i;
     }
 }
